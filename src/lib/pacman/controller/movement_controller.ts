28bdb792e04b52b49602147c6941ff5fda51adf8
Rafel Llobet <llobde@gmail.com>
2025-03-07 00:23:03 +0100

    update to improvments

--- src/lib/pacman/controller/movement_controller.ts
+++ src/lib/pacman/controller/movement_controller.ts
@@ -12,6 +12,7 @@ export class MovementController {
 	private position!: { x: number; y: number };
 	private targetPosition!: { x: number; y: number };
 	private board: PacmanBoard;
+	private bufferChangePosition = false;
 
 	constructor(board: PacmanBoard, startTile: Tile) {
 		this.board = board;
@@ -21,8 +22,9 @@ export class MovementController {
 		window.addEventListener('keydown', this.onKeyDown.bind(this));
 	}
 
-	setInTile(tile: Tile) {
+	setInTile(tile: Tile, setPosition: boolean = true) {
 		this.inTile = tile;
+		if (!setPosition) return;
 		this.position = tile.getPositionInPixels();
 	}
 
@@ -31,81 +33,98 @@ export class MovementController {
 		this.targetPosition = tile.getPositionInPixels();
 	}
 
+	get previousVelocity() {
+		return { x: this.prevDirection.x * this.speed, y: this.prevDirection.y * this.speed };
+	}
+
 	getDirectionOnKeyDown(event: KeyboardEvent): Direction | null {
 		if (event.key === 'ArrowUp') return { x: 0, y: -1 };
 		if (event.key === 'ArrowDown') return { x: 0, y: 1 };
-		if (event.key === 'ArrowLeft') return { x: -1, y: 0 };
 		if (event.key === 'ArrowLeft') return { x: -1, y: 0 };
 		if (event.key === 'ArrowRight') return { x: 1, y: 0 };
 		return null;
 	}
 
+	sameDirection(direction: Direction): boolean {
+		return direction.x === this.direction.x && direction.y === this.direction.y;
+	}
+
 	private onKeyDown(event: KeyboardEvent): void {
 		let newDirection = this.getDirectionOnKeyDown(event);
 		if (newDirection === null) return;
-		console.log('New direction', newDirection);
-		if (this.canMove(this.inTile, newDirection) && newDirection !== this.prevDirection) {
+		if (this.canMove(this.inTile, newDirection) && !this.sameDirection(newDirection)) {
+			this.bufferChangePosition = true;
 			this.prevDirection = this.direction;
 			this.direction = newDirection;
 			this.targetTile = this.board.getLastTilePathInDirection(this.inTile, this.direction)!;
-			console.log('Move to tile', this.targetTile);
 			this.setTargetTile(this.targetTile);
 			this.velocity.x = newDirection.x * this.speed;
 			this.velocity.y = newDirection.y * this.speed;
+			console.log('Velocity', this.velocity);
+			console.log('Precious velocity', this.previousVelocity);
 		}
 	}
 
+	private isInMiddleOfTile(): boolean {
+		if (this.direction.x == 1) {
+			if (this.position.x > this.inTile.getPositionInPixels().x) {
+				return true;
+			}
+		} else if (this.direction.x == -1) {
+			if (this.position.x < this.inTile.getPositionInPixels().x) {
+				return true;
+			}
+		}
+		if (this.direction.y == 1) {
+			if (this.position.y > this.inTile.getPositionInPixels().y) {
+				return true;
+			}
+		} else if (this.direction.y == -1) {
+			if (this.position.y < this.inTile.getPositionInPixels().y) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private canMove(inTile: Tile, toDirection: Direction): boolean {
-		// let nextTile: Tile | null = this.board.getAdjacentTileInDirection(inTile, toDirection);
-		// if (!nextTile) return false;
-		// if (nextTile.isWall()) {
-		// 	let nextTilePosition = nextTile.getPositionInPixels();
-		// 	console.log('Direction', this.direction);
-		// 	console.log('Next tile position', nextTilePosition);
-		// 	console.log('Current position', this.position);
-		// 	if (this.direction.y == -1) {
-		// 		console.log('UP');
-		// 		if (this.position.y > nextTilePosition.y) {
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// 	if (this.direction.y == 1) {
-		// 		console.log('DOWN');
-		// 		if (this.position.y < nextTilePosition.y) {
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// 	if (this.direction.x == -1) {
-		// 		console.log('LEFT');
-		// 		if (this.position.x > nextTilePosition.x) {
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// 	if (this.direction.x == 1) {
-		// 		console.log('RIGHT');
-		// 		if (this.position.x < nextTilePosition.x) {
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// 	return false;
-		// }
-		// return true;
+		let nextTile: Tile | null = this.board.getAdjacentTileInDirection(inTile, toDirection);
+		if (!nextTile) return false;
+		if (nextTile.isWall() && this.isInMiddleOfTile()) {
+			return false;
+		}
+		return true;
+	}
+
+	private get inBuffer() {
+		return (
+			this.bufferChangePosition && (this.previousVelocity.x !== 0 || this.previousVelocity.y !== 0)
+		);
 	}
 
 	move(delta: number): { x: number; y: number } {
 		let inTile = this.board.getTileFromPixel(this.position);
-		if (!inTile) {
-			return { x: this.position.x, y: this.position.y };
-		}
-		let nextTile = this.board.getAdjacentTileInDirection(inTile, this.direction);
-		if (nextTile && this.canMove(nextTile, this.direction)) {
-			// this.setInTile(nextTile);
-			this.position.x += this.speed * delta * this.direction.x;
-			this.position.y += this.speed * delta * this.direction.y;
+		if (inTile) {
+			this.setInTile(inTile, false);
+			if (this.canMove(this.inTile, this.direction)) {
+				// if (this.inBuffer) {
+				// 	console.log('In buffer');
+				// 	if (!this.isInMiddleOfTile()) {
+				//         console.log('Previous velocity', this.previousVelocity);
+				// 		this.position.x += this.previousVelocity.x * delta;
+				// 		this.position.y += this.previousVelocity.y * delta;
+				// 	} else {
+				// 		this.bufferChangePosition = false;
+				// 		this.position.x += this.velocity.x * delta;
+				// 		this.position.y += this.velocity.y * delta;
+				// 	}
+				// } else {
+				// 	this.position.x += this.velocity.x * delta;
+				// 	this.position.y += this.velocity.y * delta;
+				// }
+				this.position.x += this.velocity.x * delta;
+				this.position.y += this.velocity.y * delta;
+			}
 		}
 		return { x: this.position.x, y: this.position.y };
 	}
